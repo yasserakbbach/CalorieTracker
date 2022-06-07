e42e6f86bdb12d28b012aeebcbcf243cb2995679
Yasser Akbbach <akbbach.yasser@gmail.com>
2022-06-07 21:27:49 +0100

    Added OpenFoodApi and TrackerDataModule. Added Nutriments, Product and SearchDto.

---
+++ tracker/tracker_data/src/main/java/com/yasserakbbach/tracker_data/di/TrackerDataModule.kt
@@ -0,0 +1,42 @@
+package com.yasserakbbach.tracker_data.di
+
+import com.yasserakbbach.tracker_data.remote.OpenFoodApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TrackerDataModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }
+            ).build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(OpenFoodApi.BASE_URL)
+            .client(client)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideOpenFoodApi(retrofit: Retrofit): OpenFoodApi =
+        retrofit.create()
+}
---
+++ tracker/tracker_data/src/main/java/com/yasserakbbach/tracker_data/remote/dto/SearchDto.kt
@@ -0,0 +1,5 @@
+package com.yasserakbbach.tracker_data.remote.dto
+
+data class SearchDto(
+    val products: List<Product>,
+)
