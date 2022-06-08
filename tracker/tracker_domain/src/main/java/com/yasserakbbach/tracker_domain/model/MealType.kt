2b0936894e4e099d65aaa6c735086ded4b5fc1d2
Yasser Akbbach <akbbach.yasser@gmail.com>
2022-06-08 22:16:14 +0100

    Added TrackerRepository, TrackableFood, TrackedFood and MealType. Added Coroutines to tracker-domain layer

---
+++ tracker/tracker_domain/src/main/java/com/yasserakbbach/tracker_domain/model/MealType.kt
@@ -0,0 +1,26 @@
+package com.yasserakbbach.tracker_domain.model
+
+sealed class MealType(val name: String) {
+
+    object Breakfast: MealType(BREAKFAST)
+    object Lunch: MealType(LUNCH)
+    object Dinner: MealType(DINNER)
+    object Snack: MealType(SNACK)
+
+    companion object {
+
+        private const val BREAKFAST = "breakfast"
+        private const val LUNCH = "lunch"
+        private const val DINNER = "dinner"
+        private const val SNACK = "snack"
+
+        fun fromString(name: String): MealType =
+            when(name) {
+                BREAKFAST -> Breakfast
+                LUNCH -> Lunch
+                DINNER -> Dinner
+                SNACK -> Snack
+                else -> throw IllegalArgumentException("Unable to get $name meal type")
+            }
+    }
+}
