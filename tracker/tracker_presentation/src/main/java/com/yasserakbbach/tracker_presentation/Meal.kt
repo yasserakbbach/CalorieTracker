9cc8ba25128db1052070738385e9c8a3f7042a71
Yasser Akbbach <akbbach.yasser@gmail.com>
2022-06-10 22:26:06 +0100

    Added default meals and TrackerOverviewState.

--- tracker/tracker_presentation/src/main/java/com/yasserakbbach/tracker_presentation/Meal.kt
+++ tracker/tracker_presentation/src/main/java/com/yasserakbbach/tracker_presentation/Meal.kt
@@ -1,10 +1,12 @@
 package com.yasserakbbach.tracker_presentation
 
 import androidx.annotation.DrawableRes
+import com.yasserakbbach.core.util.UiText
 import com.yasserakbbach.tracker_domain.model.MealType
+import com.yasserakbbach.core.R
 
 data class Meal(
-    val name: String,
+    val name: UiText,
     @DrawableRes val drawableRes: Int,
     val mealType: MealType,
     val carbs: Int = 0,
@@ -12,4 +14,27 @@ data class Meal(
     val fat: Int = 0,
     val calories: Int = 0,
     val isExpanded: Boolean = false,
+)
+
+val defaultMeals = listOf(
+    Meal(
+        name = UiText.StringResource(R.string.breakfast),
+        drawableRes = R.drawable.ic_breakfast,
+        mealType = MealType.Breakfast,
+    ),
+    Meal(
+        name = UiText.StringResource(R.string.lunch),
+        drawableRes = R.drawable.ic_lunch,
+        mealType = MealType.Lunch,
+    ),
+    Meal(
+        name = UiText.StringResource(R.string.dinner),
+        drawableRes = R.drawable.ic_dinner,
+        mealType = MealType.Dinner,
+    ),
+    Meal(
+        name = UiText.StringResource(R.string.snacks),
+        drawableRes = R.drawable.ic_snack,
+        mealType = MealType.Snack,
+    ),
 )
