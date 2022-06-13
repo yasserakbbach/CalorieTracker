1c26c83a91e6ae2f389410c284a6cb1451c4badb
Yasser Akbbach <akbbach.yasser@gmail.com>
2022-06-13 21:45:33 +0100

    Implemented TrackedFoodItem component and added it to TrackerOverviewScreen.

--- tracker/tracker_presentation/src/main/java/com/yasserakbbach/tracker_presentation/trackeroveriew/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/yasserakbbach/tracker_presentation/trackeroveriew/TrackerOverviewScreen.kt
@@ -1,20 +1,26 @@
 package com.yasserakbbach.tracker_presentation.trackeroveriew
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.yasserakbbach.core.util.UiEvent
 import com.yasserakbbach.core_ui.LocalSpacing
+import com.yasserakbbach.core.R
+import com.yasserakbbach.tracker_presentation.trackeroveriew.components.AddButton
 import com.yasserakbbach.tracker_presentation.trackeroveriew.components.DaySelector
 import com.yasserakbbach.tracker_presentation.trackeroveriew.components.ExpandableMeal
 import com.yasserakbbach.tracker_presentation.trackeroveriew.components.NutrientsHeader
+import com.yasserakbbach.tracker_presentation.trackeroveriew.components.TrackedFoodItem
 
 @Composable
 fun TrackerOverviewScreen(
@@ -47,7 +53,24 @@ fun TrackerOverviewScreen(
                 onToggleClick = { viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(it)) },
                 modifier = Modifier.fillMaxWidth(),
             ) {
-                Text(text = "Expand Me | TODO implement")
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = spacing.spaceSmall),
+                ) {
+                    state.trackedFoods.forEach { trackedFood ->
+                        TrackedFoodItem(
+                            trackedFood = trackedFood,
+                            onDeleteClick = { viewModel.onEvent(TrackerOverviewEvent.OnDeleteTrackedFoodClick(trackedFood)) },
+                        )
+                        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                    }
+                    AddButton(
+                        text = stringResource(id = R.string.add_meal, it.name.asString(context)),
+                        onClick = { viewModel.onEvent(TrackerOverviewEvent.OnAddFoodClick(it)) },
+                        modifier = Modifier.fillMaxWidth(),
+                    )
+                }
             }
         }
     }
